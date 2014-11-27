0b34d3a016516742ee769f28dbcefc8beb494710
Jeremy-Gossiaux <jeremy.gossiaux@hotmail.com>
2014-11-27 09:57:45 +0100

    Add decoder

--- Projet 3 - BlueJ/Decoder.java
+++ Projet 3 - BlueJ/Decoder.java
@@ -1,17 +1,29 @@
-
-/**
- * Write a description of interface Decoder here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public interface Decoder
 {
     /**
-     * An example of a method header - replace this comment with your own
-     * 
-     * @param  y    a sample parameter for a method
-     * @return        the result produced by sampleMethod 
+     * @pre data != null
+     *      data est une matrice carree, de taille 32, 64, 128 ou 256
+     *      data ne contient que des 0 et des 1
+     * @post La valeur renvoyee contient le decodage de la matrice de bits data
+     *      decodee en respectant la configuration de ce decodeur
+     *      Si la matrice contient une erreur, celle-ci est corrigee
+     * @throw DecodingException au cas ou la matrice data ne peut pas etre decodee
      */
-    int sampleMethod(int y);
+    public String decode(int [][] data);
+    
+    /**
+     * @pre data != null
+     *      data est une matrice carree, de taille 32, 64, 128 ou 256
+     *      data ne contient que des 0 et des 1
+     * @post La valeur renvoyee contient true si data ne contient pas
+     *      d’ erreurs (la parite de la matrice de bits est valide),
+     *      et false sinon
+    */
+    public boolean check(int [][] data);
+    
+    /**
+     * @pre -
+     * @post La valeur renvoyee contient la configuration de ce decodeur
+    */
+    public Configuration getConfiguration();
 }
