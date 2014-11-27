1f697a0f2069278a60171116bad8ed9ba04909dd
Robin-gielen <robin.gielen@student.uclouvain.be>
2014-11-27 10:54:25 +0100

    Uncomment testBin

--- Projet 3 - BlueJ/TestBin.java
+++ Projet 3 - BlueJ/TestBin.java
@@ -5,21 +5,7 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-// import java.io.UnsupportedEncodingException;
-// 
-// public class Test
-// {
-//     public static void main (String[] args) throws UnsupportedEncodingException
-//     {
-//         byte[] infoBin = null;
-//         String test = "this is plain text";
-//         infoBin = test.getBytes("UTF-8");
-//         for (int i = 0; i < infoBin.length();i++)
-//         {
-//             System.out.println("c:" + (char) 
-//         }
-//     }
-// }
+
 import java.io.UnsupportedEncodingException;
 
 public class TestBin {
