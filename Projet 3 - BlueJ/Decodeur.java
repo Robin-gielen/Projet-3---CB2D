ef81789d54b599098f6c5144fc5b016885778d9b
Julien Banken <julien.banken@student.uclouvain.be>
2014-11-29 23:47:54 +0100

    Encoder - Décoder
    Implémentation des class "encodeur" et "décodeur"

--- Projet 3 - BlueJ/Decodeur.java
+++ Projet 3 - BlueJ/Decodeur.java
@@ -9,24 +9,94 @@ import barcode2d.BarCode2DReader;
  */
 public class Decodeur implements Decoder
 {
-    
-    public String decode(int[][] data)
+    /* 
+    * @pre - data != null
+    *        data est une matrice carree, de taille 32, 64, 128 ou 256
+    *        data ne contient que des 0 et des 1
+    * @post - La valeur renvoyee contient le decodage de la matrice de bits data
+     *        decodee en respectant la configuration de ce decodeur
+    */
+    public String decode(int[][] data) throws DecodingException
     {
-        
+        StringBuffer msg = new StringBuffer(); 
+        if (check (data)) {
+            for (int i = 1; i < data.length; i++) {
+                for (int j = 1; j < data[i].length; j++) {
+                    msg.append(data[i][j]); 
+                }
+            }
+        }
+        return convertir(msg.toString());
     }
-    
+	
+    /* 
+    * @pre - data != null
+    *        data est une matrice carree, de taille 32, 64, 128 ou 256
+    *        data ne contient que des 0 et des 1
+    * @post - Renvoi une chaine de caracteres qui correspond au code binaire 
+    */
+	public String convertir (String msg) 
+	{
+        StringBuffer texte = new StringBuffer(); 
+        // On selectionne les bytes 8 par 8 : 
+        for (int i = 0; i < msg.length()/8; i++) {
+            int lettre = Integer.parseInt(msg.substring(8*i,(i+1)*8),2);
+            texte.append((char)(lettre));
+        }
+        return texte.toString();
+    }
+	
+    /* 
+    * @pre - data != null
+	*		 data est une matrice carree, de taille 32, 64, 128 ou 256
+    *        data ne contient que des 0 et des 1
+    * @post - La valeur renvoyee contient true si data ne contient pas
+     *        d’erreurs (la parite de la matrice de bits est valide),
+     *        et false sinon
+    */
     public boolean check(int[][] data)
     {
-        
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                if ((data[i][j] != 0) && (data[i][j] != 1)) {
+                    return false; 
+                }  
+            }
+        }
+        return true;
     }
-    
+	
+    /* 
+    * @pre - 
+    * @post - 
+    */
     public Configuration getConfiguration()
     {
         
     }
-    
+ 
+    /* 
+    * @pre - image != null
+    * @post - 
+    */ 
     public int[][] lecteur (String path)
     {
-        
+		// Lire une image externe : 
+        Image code2D = ImageIO.read(new File (path)); 
+    }
+	
+	/* 
+    * @pre - Type du tableau : int
+	*		data != null
+    * @post - Affiche l'integralite du tableau
+    */
+	public void afficher (int[][]data) 
+	{
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data.length; j++) {
+                System.out.print(data[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
--- Projet 3 - BlueJ/Encodeur.java
+++ Projet 3 - BlueJ/Encodeur.java
@@ -9,16 +9,154 @@ import barcode2d.*;
 public class Encodeur implements Encoder
 {
     /**
-     * 
+     * @pre - 
+	 * @post -
      */
     public int[][] encode (String msg)
     {
-        
+        StringBuffer chaineBinaire = new StringBuffer(); 
+
+        for (int i = 0; i < msg.length(); i++) {
+            if (msg.contains(" ")){
+                if (msg.charAt(i) == ' '){
+                    chaineBinaire.append("00100000"); 
+                }
+                else {
+                    chaineBinaire.append("0").append(Integer.toBinaryString(msg.charAt(i)));
+                }   
+            }
+            else {
+                chaineBinaire.append("0").append(Integer.toBinaryString(msg.charAt(i)));
+            }
+        }
+        return tableauBinaire(chaineBinaire.toString());
     }
-    
+	
     /**
-     * 
+     * @pre - 
+	 * @post -
      */
+	public int[][] tableauBinaire (String chaineBinaire){
+
+        int [][]tableau = new int [32][32]; 
+        int indexe = 0;
+
+        int place = 1;
+        int indice = 0; 
+
+		/*
+		System.out.println();
+        System.out.println("Dimension : " + tableau.length);
+        System.out.println("Chaine binaire :" + chaineBinaire);
+        System.out.println ("Taille du String : " + chaineBinaire.length()); 
+        System.out.println(); 
+		*/
+		
+        // Generation du tableau :
+
+        for (int i = 1; i < tableau.length; i++){
+            for (int j = 1; j < tableau.length; j++){
+
+                int c = Character.getNumericValue(chaineBinaire.charAt(indexe));
+                tableau[j][i] = c; 
+                indexe ++; 
+
+                if (indexe >= chaineBinaire.length()){
+                    j = tableau.length; 
+                    i = tableau.length;
+                }
+            }
+        }    
+
+        // Generation des bytes de parite :
+
+        // Paire = 0
+        // Impaire = 1; 
+
+        while (place < tableau.length) {
+            // Ligne : 
+            if (isPaire(tableau, place, true)) { 
+                tableau [place][0] = 0; 
+            }
+            else{
+                tableau [place][0] = 1; 
+            }
+            // Colonne : 
+            if (isPaire(tableau, place, false)) { 
+                tableau [0][place] = 0; 
+            }
+            else{
+                tableau [0][place] = 1; 
+            }
+
+            place++; 
+        }
+
+        // Byte de parite (0;0) :
+
+        if (isPaire (tableau,indice, true) == isPaire (tableau, indice, false)) { 
+
+            if (isPaire (tableau,indice, true)) {
+                // Paire
+                tableau [0][0] = 1;
+            }
+            else {
+                // Impaire
+                tableau [0][0] = 1;
+            }
+        }
+        else {
+            System.out.println("Erreur : Les bytes de parite ne correspondent pas"); 
+        }
+        return tableau; 
+    }
+
+    /**
+     * @pre - 
+	 * @post -
+     */
+	public boolean isPaire(int [][] tableau, int place, boolean isLine){
+
+        int compteur = 0;
+        boolean isPaire = false;
+
+        if(isLine){
+            for (int i = 1; i < tableau.length; i++){
+                if(tableau[place][i] == 1){
+                    compteur ++;
+                }
+            }
+        }
+        else{
+            for (int i = 1; i < tableau.length; i++){
+                if(tableau[i][place] == 1){
+                    compteur ++;
+                }
+            }
+        }  
+
+        return ((compteur % 2) == 0);
+    }
+
+    /**
+     * @pre - 
+	 * @post -
+     */	
+    public static void afficher (int[][]tableau) {
+
+        for (int i = 0; i < tableau.length; i++) {
+            for (int j = 0; j < tableau.length; j++) {
+                System.out.print(tableau[j][i]);
+            }
+            System.out.println();
+        }
+
+    }
+	
+    /**
+     * @pre - 
+	 * @post -
+     */	
     public Configuration getConfiguration()
     {
         
