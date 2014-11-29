1237c80f37350dcab7d144c1fd8ca067aa8434f7
Robin-gielen <robin.gielen@student.uclouvain.be>
2014-11-29 16:41:21 +0100

    New class + adjustement (Constantes)
    Ajout d'une classe spécifique aux constantes pour plus de facilité dans
    la globalité d'utilisation

--- Projet 3 - BlueJ/Config.java
+++ Projet 3 - BlueJ/Config.java
@@ -6,8 +6,6 @@ public class Config implements Configuration
     int dataType;
     int compressionMode;
     String dixBitsConfig;
-    String [] tab1 = {"000","001","010","011","100","101","110","111"};
-    String [] tab2 = {"0000","0001","0010","0011","0100","0101","0110","0111","1000","1001","1010","1011","1100","1101","1110","1111"};
     
     /**
      * Ce constructeur prend en argument une chaine de 10 caracteres (1 et 0) qui sont tires du code-barre en train d etre lu.
@@ -26,23 +24,23 @@ public class Config implements Configuration
     
     public int getSize(){
         if (dixBitsConfig != null){
-            size = index(tab1,0,3);
+            size = index(Constantes.indexParam1,0,3);
         }
         return size;
     }
     
     public int getDataType(){
         if (dixBitsConfig != null){
-            dataType = index(tab2,3,7);
+            dataType = index(Constantes.indexParam2,3,7);
         }
         return dataType;
     }
     
     public int getCompressionMode(){
         if (dixBitsConfig != null){
-            compressionMode = index(tab1,7,10);
-    	}
-    	return compressionMode;
+            compressionMode = index(Constantes.indexParam1,7,10);
+        }
+        return compressionMode;
     }
     
     public int index (String [] tab, int begin, int end){
@@ -51,10 +49,10 @@ public class Config implements Configuration
         int fin = tab.length;
         for (int i = 0; i < fin; i++){
             if (tab[i].equals(temp)){
-    			index = i;
-    			i = fin;
-    		}
-    	}
-    	return index;
+                index = i;
+                i = fin;
+            }
+        }
+        return index;
     }
 }
