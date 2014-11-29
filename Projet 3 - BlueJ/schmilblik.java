d7d06baf5595eff484cd49c9033ac56c67ff0537
Jeremy-Gossiaux <jeremy.gossiaux@hotmail.com>
2014-11-29 15:31:45 +0100

    Last up before leave

--- Projet 3 - BlueJ/schmilblik.java
+++ Projet 3 - BlueJ/schmilblik.java
@@ -1,16 +1,16 @@
 public class schmilblik{
-    public static boolean isPaire(int [][] tab, int place, boolean isLine){
+    public static boolean isPaire(int [][] tab, int place, boolean isLine,Configuration config){
         int compteur = 0;
         boolean isPaire = false;
         if(isLine){
-            for (int i = 1; i < truck.getSize; i++){
+            for (int i = 1; i < config.getSize(); i++){
                 if(tab[place][i]==1){
                     compteur ++;
                 }
             }
         }
         else{
-            for (int i = 1; i < truck.getSize; i++){
+            for (int i = 1; i < config.getSize(); i++){
                 if(tab[i][place]==1){
                     compteur ++;
                 }
