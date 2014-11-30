4036a59022bec3d4913b989919ef014ce8fea2c9
Julien Banken <julien.banken@student.uclouvain.be>
2014-11-30 17:24:48 +0100

    Ajout de la méthode détection
    Cette méthode détecte les anomalies liées aux bits de parité...
    Méthode check : Vérifie si la matrice respecte les dimensions (32,64,128
    et 256)
    
    Note : On pourrait stocker les erreurs dans un tableau à deux dimensions
    pour les traiter dans une autre méthode

--- Projet 3 - BlueJ/Decodeur.java
+++ Projet 3 - BlueJ/Decodeur.java
@@ -14,9 +14,12 @@ public class Decodeur implements Decoder
     *        data est une matrice carree, de taille 32, 64, 128 ou 256
     *        data ne contient que des 0 et des 1
     * @post - La valeur renvoyee contient le decodage de la matrice de bits data
-     *        decodee en respectant la configuration de ce decodeur
+    *        decodee en respectant la configuration de ce decodeur
+	* A ajouter : 
+	*		Si la matrice contient une erreur, celle-ci est corrigee
+    * @throw DecodingException au cas ou la matrice data ne peut pas etre decodee
     */
-    public String decode(int[][] data) throws DecodingException
+    public String decode(int[][] data)
     {
         StringBuffer msg = new StringBuffer(); 
         if (check (data)) {
@@ -58,12 +61,86 @@ public class Decodeur implements Decoder
     {
         for (int i = 0; i < data.length; i++) {
             for (int j = 0; j < data[i].length; j++) {
+                // On verifie si le tableau contient des 0 et des 1
                 if ((data[i][j] != 0) && (data[i][j] != 1)) {
                     return false; 
                 }  
+                // On verifie si c'est une matrice carree et si ses dimensions sont egales à 32, 64, 128 ou 256  
+                if ((data.length != data[i].length) || (data[i].length != 32) && (data[i].length != 64) && (data[i].length != 128) && (data[i].length != 256)){
+                    return false; 
+                }
             }
         }
         return true;
+    }
+	
+    /* 
+    * @pre - data != null
+	*		 data est une matrice carree, de taille 32, 64, 128 ou 256
+    *        data ne contient que des 0 et des 1
+    * @post - Detecte les erreurs : Affiche les lignes et les colonnes des anomalies
+    */
+	public static void detection (int [][]data) {
+
+        int compteurColonne = 0; 
+        int compteurLigne = 0;
+        int indicePariteColonne;
+        int indicePariteLigne;
+        
+        // i = colonne
+        // j = ligne
+
+        // Les lignes de parite horizontale :
+        for (int i = 1; i < data.length; i ++){
+            // On calcul le nombre de 1 (colonne)
+            for (int j = 1; j < data[i].length; j++){
+                if(data[j][i] == 1){
+                    compteurColonne ++;
+                }
+            }
+            // Si le nombre de 1 est paire :
+            if (compteurColonne%2 == 0) {
+                indicePariteColonne = 0; 
+            }
+            // Si le nombre de 1 est impaire :
+            else {
+                indicePariteColonne = 1;
+            }
+
+            // On reset le compteur  
+            compteurColonne = 0;
+
+            // Si le bit de parite est different de ce que nous venons de calculer
+            if (data[0][i] != indicePariteColonne ) {
+
+                System.out.println("Erreur trouvee a la colonne : " + (i + 1));
+
+                for (int k = 1 ; k < data.length; k++) {
+                    // On calcul le nombre de 1 (ligne)
+                    for (int l = 1; l < data[k].length; l++) {
+
+                        if (data[k][l] == 1) {
+                            compteurLigne++; 
+                        }
+                    }
+                    // Si le nombre de 1 est paire
+                    if (compteurLigne%2 == 0) {
+                        indicePariteLigne = 0; 
+                    }
+                    // Si le nombre de 1 est impaire
+                    else {
+                        indicePariteLigne = 1;
+                    }
+
+                    // On reset le compteur 
+                    compteurLigne = 0; 
+
+                    if (data[k][0] != indicePariteLigne) {
+                        System.out.println("Erreur trouvee a la ligne : " + (k + 1));
+                    } 
+                }  
+            }
+        }
     }
 	
     /* 
@@ -81,8 +158,7 @@ public class Decodeur implements Decoder
     */ 
     public int[][] lecteur (String path)
     {
-		// Lire une image externe : 
-        Image code2D = ImageIO.read(new File (path)); 
+
     }
 	
 	/* 
